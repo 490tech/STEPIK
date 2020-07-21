3a1dd4e223dff34c4043643d499ab1b0a71a8b0c
490tech <66438386+490tech@users.noreply.github.com>
2020-07-21 19:40:35 +0300

    Add files via upload

--- 1.12.3.py
+++ 1.12.3.py
@@ -1,16 +1,19 @@
-x=int(input())
-y=int(input())
-
-
-
-
-
-
-
-print (x*y)
-print (x+y)
-print (x/y)
-print (x-y)
-print (x%y)
-print (x**y)
-print (x//y)
+a = float(input())
+b = float(input())
+c = str(input())
+if (c == '+'):
+    print (a + b)
+if (c=='-'):
+    print (a - b)
+if ( c=='*'):
+    print(a*b)
+if (c=='pow'):
+    print(a**b) 
+if (b==0.0) and (c=='mod' or (c=='/') or (c=='div')):
+    print('Деление на 0!')      
+elif (c=='/'):
+    print (a/b)
+elif (c=='mod'):
+    print (a%b)      
+elif (c=='div'):
+    print ( a//b)
