72f825b8df370c93922f3582bba83a1c543c825e
490tech <66438386+490tech@users.noreply.github.com>
2020-07-22 04:39:59 +0300

    Update 1.12.4.py

--- 1.12.4.py
+++ 1.12.4.py
@@ -1,19 +1,17 @@
-a = float(input())
-b = float(input())
-c = str(input())
-if (c == '+'):
-    print (a + b)
-if (c=='-'):
-    print (a - b)
-if ( c=='*'):
-    print(a*b)
-if (c=='pow'):
-    print(a**b) 
-if (b==0.0) and (c=='mod' or (c=='/') or (c=='div')):
-    print('Деление на 0!')      
-elif (c=='/'):
-    print (a/b)
-elif (c=='mod'):
-    print (a%b)      
-elif (c=='div'):
-    print ( a//b)
+z = str(input()) #тип комнаты
+if z=="треугольник":
+  a = float(input()) #сторона1
+  b = float(input()) #сторона2
+  c = float(input()) #сторона3
+  y = (a+b+c)/2
+  print ((y*(y-a)*(y-b)*(y-c))**0.5)
+
+if z=="прямоугольник":
+  a = float(input()) #сторона1
+  b = float(input()) #сторона2
+  print ((a*b))
+  
+if (z == 'круг'):
+  r = float(input()) #радиус
+  p = 3.14 #пи
+  print (p*r**2)
