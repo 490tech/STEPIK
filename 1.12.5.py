744f91ea291cbf919dc6dd3313b515c50902eef2
490tech <66438386+490tech@users.noreply.github.com>
2020-07-23 15:13:53 +0300

    Add files via upload

---
+++ 1.12.5.py
@@ -0,0 +1,11 @@
+a = int(input()) #число 1 
+b = int(input()) #число 2
+c = int(input()) #число 3
+
+nums=[a,b,c]
+nums.sort()
+#nums2=sorted(nums)
+#print (nums2)
+print (nums[2])
+print (nums[0])
+print (nums[1])
